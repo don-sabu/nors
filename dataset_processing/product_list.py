0311b1c81ca7018508aed7f51cd3752a04675e62
don-sabu <donsabu@tutanota.com>
2024-04-09 07:36:30 +0530

    updated product_list.py

--- dataset_processing/product_list.py
+++ dataset_processing/product_list.py
@@ -1,15 +1,23 @@
+import os
 import pandas as pd
 
 
 def generate_unique_product_list():
-    unique_products = set()
-    for user_id in range(1, 3):  # Update the range according to the number of users
-        filename = f"user{user_id}.csv"
-        user_df = pd.read_csv(filename)
-        unique_products.update(user_df['product'].unique())
+    products = {}
+    users = os.listdir("data/")
+    for user in users:
+
+        train = pd.read_csv("data/" + user + "/train.csv")
+        test = pd.read_csv("data/" + user + "/test.csv")
+        for index, row in train.iterrows():
+            products[row['product_id']] = row['name']
+        for index, row in test.iterrows():
+            products[row['product_id']] = row['name']
+
 
     # Create a DataFrame for unique products
-    products_df = pd.DataFrame({'product': sorted(unique_products)})
+    products_df = pd.DataFrame({'id': list(products.keys()),
+                                'name':list(products.values())})
 
     # Save the unique products to a CSV file
     products_df.to_csv('products.csv', index=False)
